22594140b55edc4764c9b43625eebda66381ca1b
Thomas Fischer <tf@media-nox.de>
2025-01-06 15:37:31 +0100

    19

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,21 @@ module.exports = {
     extend: {
       keyframes: {
         wavey: {
-          "0%, 100%" : {
+          "0%, 100%": {
             transform: "scaleY(0.5)",
           },
           "50%": {
             transform: "scaleY(1.5)",
-          }
-        }
+          },
+        },
       },
       animation: {
         wavey: "wavey 1000ms linear infinite",
-      }
-    }
+      },
+    },
   },
   plugins: [
-    require("./plugins/openVariant")
+    require("./plugins/openVariant"),
+    require("./plugins/animationDelay"),
   ],
 };
